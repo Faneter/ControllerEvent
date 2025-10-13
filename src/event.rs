8abad7dd8a9bacb801d5cbca63a8b5a08f3219cf
Faneter <Faneter@outlook.com>
2025-10-13 12:55:21 +0800

    略完善事件处理机制，并测试成功

--- src/event.rs
+++ src/event.rs
@@ -1,8 +1,17 @@
 use std::collections::HashMap;
 
 use enigo::{Button, Key};
+use gilrs::ev;
 
-pub enum Input {}
+use crate::{
+    controller::{GamepadState, KeyState},
+    event,
+};
+
+#[derive(Eq, Hash, PartialEq)]
+pub enum Input {
+    Button(gilrs::Button),
+}
 
 pub enum Event {
     KeyClick(Key),         // 按下并释放单个按键
@@ -25,4 +34,36 @@ pub struct Binder {
     // 特殊模式配置
     toggle_modes: HashMap<Input, (Event, Event)>, // 切换模式
     hold_modes: HashMap<Input, (Event, Event)>,   // 按住模式
+}
+
+impl Binder {
+    pub fn new() -> Self {
+        Self {
+            mappings: HashMap::new(),
+            combo_mappings: HashMap::new(),
+            toggle_modes: HashMap::new(),
+            hold_modes: HashMap::new(),
+        }
+    }
+
+    pub fn add_mapping(&mut self, input: Input, event: Event) {
+        self.mappings.insert(input, event);
+    }
+
+    pub fn handle_events(&self, gamepad_state: &GamepadState, input: &Input) {
+        if let Some(event) = self.mappings.get(input) {
+            match input {
+                Input::Button(button) => {
+                    if let Some(state) = gamepad_state.get_button_state(button) {
+                        if let KeyState::Key(true) = state {
+                            if let Event::OtherEvent(func) = event {
+                                func();
+                            }
+                        }
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,58 +7,47 @@ use std::time::Duration;
 
 use gilrs::{EventType, Gilrs};
 
+use crate::event::Binder;
+
 fn main() {
-    // let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
     let mut gilrs = Gilrs::new().unwrap();
 
     let mut gamepad_state = GamepadState::new();
+    let mut binder = Binder::new();
+
+    binder.add_mapping(
+        event::Input::Button(gilrs::Button::Select),
+        event::Event::OtherEvent(Box::new(|| println!("Hello"))),
+    );
 
     let exit_requested = false;
 
-    let mut last_event: Option<EventType> = None;
-
     while !exit_requested {
         while let Some(event) = gilrs.next_event() {
-            println!("{:?}", event);
-            last_event = Some(event.event);
+            // println!("{:?}", event);
             match event.event {
                 EventType::ButtonPressed(button, _) => {
                     gamepad_state.update_button(button, KeyState::Key(true));
-                }
-                EventType::ButtonReleased(button, _) => {
-                    gamepad_state.update_button(button, KeyState::Key(false));
-                }
-                EventType::ButtonChanged(trigger, value, _) => {
-                    gamepad_state.update_button(trigger, KeyState::Trigger(value));
-                }
-                EventType::AxisChanged(axis, value, _) => {
-                    gamepad_state.update_axis(axis, KeyState::Axis(value));
-                }
-                _ => (),
-            }
-        }
-
-        // 事件处理
-
-        // 事件处理结束
-        if let Some(event) = last_event {
-            match event {
-                EventType::ButtonPressed(button, _) => {
+                    binder.handle_events(&gamepad_state, &event::Input::Button(button));
                     gamepad_state.update_last_button(button, KeyState::Key(true));
                 }
                 EventType::ButtonReleased(button, _) => {
+                    gamepad_state.update_button(button, KeyState::Key(false));
+                    binder.handle_events(&gamepad_state, &event::Input::Button(button));
                     gamepad_state.update_last_button(button, KeyState::Key(false));
                 }
                 EventType::ButtonChanged(trigger, value, _) => {
+                    gamepad_state.update_button(trigger, KeyState::Trigger(value));
                     gamepad_state.update_last_button(trigger, KeyState::Trigger(value));
                 }
                 EventType::AxisChanged(axis, value, _) => {
+                    gamepad_state.update_axis(axis, KeyState::Axis(value));
                     gamepad_state.update_last_axis(axis, KeyState::Axis(value));
                 }
                 _ => (),
             }
         }
-
         std::thread::sleep(Duration::from_millis(1));
     }
 }
